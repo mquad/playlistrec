a150309b78b31442c08b33ad27554e8360ba6897
andreacondorelli <andreacondorelli@users.noreply.github.com>
2015-06-26 10:09:25 +0200

    Update eval.py
    Added a new version of computeMetrics

--- spark-scripts/eval.py
+++ spark-scripts/eval.py
@@ -8,7 +8,7 @@ import re
 from os import path
 
 
-def computeMetrics(conf):
+def computeMetrics_bck(conf):
     basePath = path.join("s3n://", conf['general']['bucketName'], conf['general']['clientname'])
     # basePath = "s3n://" + conf['general']['bucketName'] + "/"+conf['general']['clientname']+"/"
     splitPath = path.join(basePath, conf['split']['name'])
@@ -64,6 +64,66 @@ def computeMetrics(conf):
         temp['linkedinfo']['subjects'][0]['splitName'] = conf['split']['name']
         temp['linkedinfo']['subjects'][0]['algoName'] = conf['algo']['name']
         result.append(temp)
+    metricsPath = path.join(confPath, conf['evaluation']['name'], "metrics")
+    (sc
+     .parallelize(result)
+     .map(lambda x: json.dumps(x))
+     .repartition(1)
+     .saveAsTextFile(metricsPath))
+    print "%s successfully written to %s" % (conf['evaluation']['name'], metricsPath)
+    
+    
+def computeMetrics(conf):
+    basePath = path.join("s3n://", conf['general']['bucketName'], conf['general']['clientname'])
+    # basePath = "s3n://" + conf['general']['bucketName'] + "/"+conf['general']['clientname']+"/"
+    splitPath = path.join(basePath, conf['split']['name'])
+    # splitPath = basePath+conf['split']['name']+"/"
+    GTpath = path.join(splitPath, "GT")
+    # GTpath = splitPath+"GT"
+
+    algo_conf = conf['algo']['name'] + '_' + \
+        '#'.join([str(v) for k, v in conf['algo']['props'].iteritems() if not k.startswith('libFM')])
+    algo_conf = re.sub(r'[^A-Za-z0-9#_]', '', algo_conf)
+
+    confPath = path.join(splitPath, 'Rec', algo_conf)
+    recPath = path.join(confPath, "recommendations")
+    # recPath = splitPath+"/Rec/"+ conf['algo']['name']+"/recommendations/"
+
+    gtRDD = sc.textFile(GTpath).map(lambda x: json.loads(x)).persist(StorageLevel.MEMORY_AND_DISK)
+    recRDD = sc.textFile(recPath).map(lambda x: json.loads(x)).persist(StorageLevel.MEMORY_AND_DISK)
+
+    recommendationRDD = recRDD \
+        .flatMap(lambda x: ([(x['id'], (k['id'], k['rank'], x)) for k in x['linkedinfo']['response']]))
+
+    groundTruthRDD = gtRDD \
+        .flatMap(lambda x: ([(x['linkedinfo']['gt'][0]['id'], (k['id'], x)) for k in x['linkedinfo']['objects']]))
+
+    groundTruthRDD = gtRDD.filter(lambda x: len(x['linkedinfo']['objects']) > = k)\
+            .map(lambda x: ([(x['linkedinfo']['gt'][0]['id'], 
+                              (x['linkedinfo']['objects'][k]['id'],k)) for k in range(len(x['linkedinfo']['objects']))]))
+    hitRDDPart = recommendationRDD.join(groundTruthRDD).filter(lambda x: x[1][0][0] == x[1][1][0])
+    hitRDD = hitRDDPart.map(lambda x: (x[0],x[1][0][1],1.0,x[1][1][1])).persist(StorageLevel.MEMORY_AND_DISK)
+    result = []
+    
+    for k in [1,2,5,10]:
+        totRec = float(groundTruthRDD.filter(lambda x: x[1][1]<=k).count())
+        ## recall@N
+        for n in conf['evaluation']['metric']['prop']['N']:
+            temp = {}
+            temp['type'] = 'metric'
+            temp['id'] = -1
+            temp['ts'] = time.time()
+            temp['properties'] = {}
+            temp['properties']['name'] = conf['evaluation']['name']+"@"+str(k)
+            temp['evaluation'] = {}
+            temp['evaluation']['N'] = n
+            temp['evaluation']['value'] = hitRDD.filter(lambda x: x[1] <n and x[3] <=k).map(lambda x: x[2]).sum()/totRec
+            temp['linkedinfo'] = {}
+            temp['linkedinfo']['subjects'] = []
+            temp['linkedinfo']['subjects'].append({})
+            temp['linkedinfo']['subjects'][0]['splitName'] = conf['split']['name']
+            temp['linkedinfo']['subjects'][0]['algoName'] = conf['algo']['name']
+            result.append(temp)
     metricsPath = path.join(confPath, conf['evaluation']['name'], "metrics")
     (sc
      .parallelize(result)
