01fd4ec21e8da150d5b25875cade3bc03e50a0c9
mquad <max.square@gmail.com>
2015-06-29 12:19:40 +0200

    test conf

--- spark-scripts/split.py
+++ spark-scripts/split.py
@@ -8,18 +8,18 @@ import json
 import boto
 from os import path
 
-def testTrainUserSplit(x,percUsTr):
-    if percUsTr * 100 <= np.random.randint(0,100): return (x,1)
-    return (x,0)
+
+def testTrainUserSplit(x, percUsTr):
+    if percUsTr * 100 <= np.random.randint(0, 100): return (x, 1)
+    return (x, 0)
 
 
-
-def creaOnlineTraining(x,noRepet,prop, GT=0, tr = 0):
+def creaOnlineTraining(x, noRepet, prop, GT=0, tr=0):
     result = list()
     REQ = {}
     REQ['type'] = 'request'
     REQ['id'] = createId(x)
-    REQ['ts'] = x['ts'] 
+    REQ['ts'] = x['ts']
     REQ['properties'] = prop
     REQ['linkedinfo'] = {}
     REQ['linkedinfo']['objects'] = []
@@ -27,9 +27,9 @@ def creaOnlineTraining(x,noRepet,prop, GT=0, tr = 0):
     alreadyAdded = set()
     conta = 0
     for k in x['linkedinfo']['objects']:
-        if  len(REQ['linkedinfo']['objects']) >= tr: break
+        if len(REQ['linkedinfo']['objects']) >= tr: break
         conta += 1
-        if k['id'] in alreadyAdded  and noRepet: continue
+        if k['id'] in alreadyAdded and noRepet: continue
         alreadyAdded.add(k['id'])
         REQ['linkedinfo']['objects'].append(k)
     REQ['linkedinfo']['subjects'] = x['linkedinfo']['subjects']
@@ -46,31 +46,30 @@ def creaOnlineTraining(x,noRepet,prop, GT=0, tr = 0):
     GT['linkedinfo']['gt'].append({})
     GT['linkedinfo']['gt'][0]['type'] = 'request'
     GT['linkedinfo']['gt'][0]['id'] = REQ['id']
-    
+
     ## QUA mi dovrebbe ritornare un elemento per ciascun evento della sessione
     if conta > len(x['linkedinfo']['objects']): return result
     for k in x['linkedinfo']['objects'][conta:]:
-        if  len(GT['linkedinfo']['objects']) >= tr: break
+        if len(GT['linkedinfo']['objects']) >= tr: break
         conta += 1
         if k['id'] in alreadyAdded and noRepet: continue
         alreadyAdded.add(k['id'])
         GT['linkedinfo']['objects'].append(k)
     result.append(GT)
-    return result 
+    return result
 
 
-
-
-def createId(user,ts=0):
+def createId(user, ts=0):
     if type(user) == dict and long(ts) == 0:
-        return long(user['linkedinfo']['subjects'][0]['id']) *100000000000 + long(user['ts'])
+        return long(user['linkedinfo']['subjects'][0]['id']) * 100000000000 + long(user['ts'])
     elif type(user) == dict and long(ts) != 0:
-        return long(user['linkedinfo']['subjects'][0]['id']) *100000000000 + long(ts)
-    else: return user *100000000000 + ts
+        return long(user['linkedinfo']['subjects'][0]['id']) * 100000000000 + long(ts)
+    else:
+        return user * 100000000000 + ts
 
 
-def gt1Creator(x,prop, mode = 'ts-1', TS = 0):
-   ## REQ
+def gt1Creator(x, prop, mode='ts-1', TS=0):
+    ## REQ
     if mode == 'ts-1' or mode == 'req':
         if mode == 'req':
             x['ts'] = TS
@@ -82,11 +81,11 @@ def gt1Creator(x,prop, mode = 'ts-1', TS = 0):
         REQ['properties'] = prop
         REQ['linkedinfo'] = {}
         REQ['linkedinfo']['objects'] = []
-        REQ['linkedinfo']['subjects'] = x['linkedinfo']['subjects'] 
+        REQ['linkedinfo']['subjects'] = x['linkedinfo']['subjects']
     if mode == 'req': return REQ
     ## GT
     if mode == 'ts-multi':
-        ident = createId(x,TS)
+        ident = createId(x, TS)
     GT = {}
     GT['type'] = x['type']
     GT['id'] = x['id']
@@ -100,7 +99,7 @@ def gt1Creator(x,prop, mode = 'ts-1', TS = 0):
     GT['linkedinfo']['gt'][0]['id'] = ident
 
     if mode == 'ts-multi': return GT
-    return (GT,REQ)
+    return (GT, REQ)
 
 
 def splitter(conf):
@@ -122,38 +121,36 @@ def splitter(conf):
         implemented modes: 'session', 'ts-1', 'ts-multi' 
     ALL :conf['split']['forceSplitCreation'] = True
     '''
-    
+
     s3 = boto.connect_s3()
     mybucket = s3.get_bucket(conf['general']['bucketName'])
-    key = mybucket.get_key(path.join(conf['split']['split'], conf['split']['name']+'_$folder$'))
+    key = mybucket.get_key(path.join(conf['split']['split'], conf['split']['name'] + '_$folder$'))
     if key and not conf['split']['forceSplitCreation']:
         print 'Split already done'
         return None
     elif key:
         for key_list in mybucket.list():
-            if conf['split']["name"]+"/" in key_list.name or conf['split']["name"]+"_$folder$" == key_list.name: 
+            if conf['split']["name"] + "/" in key_list.name or conf['split']["name"] + "_$folder$" == key_list.name:
                 mybucket.delete_key(key_list)
         print "The old split has been erased"
     ## param extraction
     mode = conf['split']['mode']
-    path = 's3n://contentwise-research-poli/split/' + conf['split']["name"]+"/"
+    path = 's3n://contentwise-research-poli/split/' + conf['split']["name"] + "/"
     minEventsPerUser = conf['split']['minEventsPerUser']
     inputFilename = conf['split']['inputData']
     TS = conf['split']['ts']
-    prop = {'reclistSize':conf['split']['reclistSize']}
+    prop = {'reclistSize': conf['split']['reclistSize']}
 
-    
-    readDataset = sc.textFile(inputFilename).map(lambda x: json.loads(x))\
-                    .map(lambda x: (x['linkedinfo']['subjects'][0]['id'],x)).persist(StorageLevel.MEMORY_AND_DISK)
-           
-    lowActUsersRDD = readDataset.map(lambda x: (x[0],1))\
-                    .reduceByKey(lambda x,y: x+y).filter(lambda x: x[1] <= minEventsPerUser).persist()
-    
+    readDataset = sc.textFile(inputFilename).map(lambda x: json.loads(x)) \
+        .map(lambda x: (x['linkedinfo']['subjects'][0]['id'], x)).persist(StorageLevel.MEMORY_AND_DISK)
+
+    lowActUsersRDD = readDataset.map(lambda x: (x[0], 1)) \
+        .reduceByKey(lambda x, y: x + y).filter(lambda x: x[1] <= minEventsPerUser).persist()
+
     readDataset2 = readDataset.subtractByKey(lowActUsersRDD).persist(StorageLevel.MEMORY_AND_DISK)
-    
+
     readDataset.unpersist()
-    
-    
+
     if mode == 'session':
         percUsTr = conf['split']['percUsTr']
         minEventPerSession = conf['split']['minEventPerSession']
@@ -161,48 +158,45 @@ def splitter(conf):
         GTlength = conf['split']['GTlength']
         minEventPerSessionTraining = conf['split']['minEventPerSessionTraining']
         minEventPerSessionTest = conf['split']['minEventPerSessionTest']
-        
-        splitTestTrain = readDataset2.map(lambda x: x[0]).distinct().map(lambda x: testTrainUserSplit(x,percUsTr))\
-                                .persist(StorageLevel.MEMORY_AND_DISK) 
-        
+
+        splitTestTrain = readDataset2.map(lambda x: x[0]).distinct().map(lambda x: testTrainUserSplit(x, percUsTr)) \
+            .persist(StorageLevel.MEMORY_AND_DISK)
+
         testUsersRDD = splitTestTrain.filter(lambda x: x[1] == 1)
         trainUsersRDD = splitTestTrain.filter(lambda x: x[1] == 0)
-        
-        readDataset2.filter(lambda x: len(x[1]['linkedinfo']['objects']) >= minEventPerSessionTraining)\
-                .join(trainUsersRDD).map(lambda x: json.dumps(x[1][0]))\
-                .saveAsTextFile(path+"train/batchTraining/")
-            
-        testRDD = readDataset2.filter(lambda x: len(x[1]['linkedinfo']['objects']) >= minEventPerSessionTraining)\
-                    .join(testUsersRDD).map(lambda x: (long(x[1][0]['ts']),x[1][0]))
-            
-        testRDD.filter(lambda x: long(x[0]) <= TS).map(lambda x: json.dumps(x[1]))\
-            .saveAsTextFile(path+"test/batchTraining/")
 
-        recAndGt = testRDD.filter(lambda x: long(x[0]) > TS)\
-                .flatMap(lambda x: creaOnlineTraining(x[1],conf['split']['excludeAlreadyListenedTest'],prop,GT=GTlength, 
-                                                      tr = onlineTrainingLength))
+        readDataset2.filter(lambda x: len(x[1]['linkedinfo']['objects']) >= minEventPerSessionTraining) \
+            .join(trainUsersRDD).map(lambda x: json.dumps(x[1][0])) \
+            .saveAsTextFile(path + "train/batchTraining/")
 
-        recAndGt.filter(lambda x: x['type'] == 'request').map(lambda x: json.dumps(x))\
-                .saveAsTextFile(path+"test/onlineTraining/")
-        recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x))\
-                .saveAsTextFile(path+"GT/")
-        
+        testRDD = readDataset2.filter(lambda x: len(x[1]['linkedinfo']['objects']) >= minEventPerSessionTraining) \
+            .join(testUsersRDD).map(lambda x: (long(x[1][0]['ts']), x[1][0]))
+
+        testRDD.filter(lambda x: long(x[0]) <= TS).map(lambda x: json.dumps(x[1])) \
+            .saveAsTextFile(path + "test/batchTraining/")
+
+        recAndGt = testRDD.filter(lambda x: long(x[0]) > TS) \
+            .flatMap(lambda x: creaOnlineTraining(x[1], conf['split']['excludeAlreadyListenedTest'], prop, GT=GTlength,
+                                                  tr=onlineTrainingLength))
+
+        recAndGt.filter(lambda x: x['type'] == 'request').map(lambda x: json.dumps(x)) \
+            .saveAsTextFile(path + "test/onlineTraining/")
+        recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x)) \
+            .saveAsTextFile(path + "GT/")
+
     if mode == 'ts-1' or mode == 'ts-multi':
         splitTestTrain = readDataset2.map(lambda x: (x[1]['ts'], x[1])).persist()
-        splitTestTrain.filter(lambda x: long(x[0]) <= TS).map(lambda x: json.dumps(x[1])).saveAsTextFile(path+"train/batchTraining/")
+        splitTestTrain.filter(lambda x: long(x[0]) <= TS).map(lambda x: json.dumps(x[1])).saveAsTextFile(
+            path + "train/batchTraining/")
         test = splitTestTrain.filter(lambda x: long(x[0]) > TS)
-        if mode == 'ts-1':  
-            recAndGt = test.flatMap(lambda x: gt1Creator(x[1],prop))
+        if mode == 'ts-1':
+            recAndGt = test.flatMap(lambda x: gt1Creator(x[1], prop))
         else:
-            recAndGt = test.map(lambda x: gt1Creator(x[1],prop,mode='ts-multi', TS = TS))\
-                            .union(test.map(lambda x: (x[1]['linkedinfo']['subjects'][0]['id'],x))\
-                                    .reduceByKey(lambda x,y: x).map(lambda x: gt1Creator(x[1][1],prop,mode='req', TS = TS)))\
-                            
-            
-        recAndGt.filter(lambda x: x['type'] == 'request').map(lambda x: json.dumps(x)).repartition(16)\
-                .saveAsTextFile(path+"test/onlineTraining/")
-        recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x)).repartition(16)\
-                    .saveAsTextFile(path+"GT/")
-    
-      
-
+            recAndGt = test.map(lambda x: gt1Creator(x[1], prop, mode='ts-multi', TS=TS)) \
+                .union(test.map(lambda x: (x[1]['linkedinfo']['subjects'][0]['id'], x)) \
+                       .reduceByKey(lambda x, y: x).map(lambda x: gt1Creator(x[1][1], prop, mode='req', TS=TS))) \
+ \
+        recAndGt.filter(lambda x: x['type'] == 'request').map(lambda x: json.dumps(x)).repartition(16) \
+            .saveAsTextFile(path + "test/onlineTraining/")
+        recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x)).repartition(16) \
+            .saveAsTextFile(path + "GT/")
