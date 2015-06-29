338cf24e035f371887aefda0664e751d6bb0e20d
andreacondorelli <andreacondorelli@users.noreply.github.com>
2015-06-29 12:23:08 +0200

    Update split.py

--- spark-scripts/split.py
+++ spark-scripts/split.py
@@ -102,7 +102,7 @@ def gt1Creator(x, prop, mode='ts-1', TS=0):
     return (GT, REQ)
 
 
-def splitter(conf):
+def splitter_bck(conf):
     '''
     Input: 
     ALL: conf = {'reclistSize' : 100, 'callParams': {}, 'excludeAlreadyListenedTest': True}
@@ -200,3 +200,92 @@ def splitter(conf):
             .saveAsTextFile(path + "test/onlineTraining/")
         recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x)).repartition(16) \
             .saveAsTextFile(path + "GT/")
+def splitter(conf):
+    prop = conf[SPLIT][PROP]
+    pathOUT = conf[SPLIT][OUT]
+    
+    minEventsPerUser = conf['split']['minEventsPerUser'] if 'minEventsPerUser' in conf['split'] else 0
+    mode = conf[MODE]
+    TS = conf[SPLIT][TS_json]
+
+    if conf[SPLIT]['type'] == list:
+        RDD = sc.parallelize([])
+        for key_list in mybucket.list():
+            if conf[SPLIT][LOCATION]+"/" in  key_list.name and '_SUCCESS' in  key_list.name: 
+                path = 's3n://'+conf[BUCKET]+"/"+ key_list.name.replace('_SUCCESS','')
+                RDD = RDD.union(sc.textFile( path))
+
+    else:
+        RDD = sc.textFile('s3n://'+conf[BUCKET]+"/"+conf[SPLIT][LOCATION])
+   
+  
+
+    readDataset = RDD.map(lambda x: json.loads(x))\
+                        .map(lambda x: (x[LINKEDINFO][SUBJECTS][0][ID],x)).persist(StorageLevel.MEMORY_AND_DISK)
+               
+    lowActUsersRDD = readDataset.map(lambda x: (x[0],1))\
+                    .reduceByKey(lambda x,y: x+y).filter(lambda x: x[1] <= minEventsPerUser).persist()
+    
+    readDataset2 = readDataset.subtractByKey(lowActUsersRDD).persist(StorageLevel.MEMORY_AND_DISK)
+    
+    readDataset.unpersist()
+
+    
+    if mode == 'session' or 'total':
+        percUsTr = conf['split']['percUsTr']
+        minEventPerSession = conf['split']['minEventPerSession']
+        onlineTrainingLength = conf['split']['onlineTrainingLength']
+        GTlength = conf['split']['GTlength']
+        minEventPerSessionTraining = conf['split']['minEventPerSessionTraining']
+        minEventPerSessionTest = conf['split']['minEventPerSessionTest']
+        
+        splitTestTrain = readDataset2.map(lambda x: x[0]).distinct().map(lambda x: testTrainUserSplit(x,percUsTr))\
+                                .persist(StorageLevel.MEMORY_AND_DISK) 
+        
+        testUsersRDD = splitTestTrain.filter(lambda x: x[1] == 1)
+        trainUsersRDD = splitTestTrain.filter(lambda x: x[1] == 0)
+        
+        readDataset2.filter(lambda x: len(x[1]['linkedinfo']['objects']) >= minEventPerSessionTraining)\
+                .join(trainUsersRDD).map(lambda x: json.dumps(x[1][0]))\
+                .saveAsTextFile(pathOUT+"train/batchTraining/") 
+        testRDD = readDataset2.filter(lambda x: len(x[1]['linkedinfo']['objects']) >= minEventPerSessionTest)\
+                        .join(testUsersRDD).map(lambda x: (long(x[1][0]['ts']),x[1][0]))
+
+        testRDD.filter(lambda x: long(x[0]) <= TS).map(lambda x: json.dumps(x[1]))\
+            .saveAsTextFile(pathOUT+"test/batchTraining/")
+        
+        if mode == 'session':
+            recAndGt = testRDD.filter(lambda x: long(x[0]) > TS)\
+                    .flatMap(lambda x: creaOnlineTraining(x[1],conf['split']['excludeAlreadyListenedTest'],prop,GT=GTlength, 
+                                                          tr = onlineTrainingLength))
+        else:
+            recAndGt = testRDD.filter(lambda x: long(x[0]) > TS)\
+                    .flatMap(lambda x: creaOnlineTraining(x[1],conf['split']['excludeAlreadyListenedTest'],prop, 
+                                                          GT = 1000000, tr=onlineTrainingLength))
+        
+
+
+
+   
+        recAndGt.filter(lambda x: x['type'] == 'request').map(lambda x: json.dumps(x))\
+                .saveAsTextFile(pathOUT+"test/onlineTraining/")
+        recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x))\
+                .saveAsTextFile(pathOUT+"GT/")
+        
+    if mode == 'ts-1' or mode == 'ts-multi':
+        splitTestTrain = readDataset2.map(lambda x: (x[1]['ts'], x[1])).persist()
+        splitTestTrain.filter(lambda x: long(x[0]) <= TS).map(lambda x: json.dumps(x[1]))\
+                    .saveAsTextFile(pathOUT+"train/")
+        test = splitTestTrain.filter(lambda x: long(x[0]) > TS)
+        if mode == 'ts-1':  
+            recAndGt = test.flatMap(lambda x: gt1Creator(x[1],prop))
+        else:
+            recAndGt = test.map(lambda x: gt1Creator(x[1],prop,mode='ts-multi', TS = TS))\
+                            .union(test.map(lambda x: (x[1]['linkedinfo']['subjects'][0]['id'],x))\
+                                    .reduceByKey(lambda x,y: x).map(lambda x: gt1Creator(x[1][1],prop,mode='req', TS = TS)))\
+                            
+            
+        recAndGt.filter(lambda x: x['type'] == 'request').map(lambda x: json.dumps(x)).repartition(16)\
+                .saveAsTextFile(pathOUT+"test/request/")
+        recAndGt.filter(lambda x: x['type'] != 'request').map(lambda x: json.dumps(x)).repartition(16)\
+                    .saveAsTextFile(pathOUT+"GT/")
